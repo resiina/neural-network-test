aead074e2d97e9acce184b0af34e9c8846901019
Lassi Rintala <lassi.rintala@gmail.com>
2021-04-27 19:53:42 +0300

    Initial testing of Bitmap library

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+#include "bitmap.h"
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    Bitmap testBitmap;
+    testBitmap.open("data/testBitmap.bmp");
+
+    PixelMatrix pixels = testBitmap.toPixelMatrix();
+
+    for (const auto & row : pixels)
+    {
+        for (const auto & pixel : row)
+        {
+            std::cout << std::to_string(pixel.blue) << std::endl;
+        }
+    }
 
     return 0;
 }
