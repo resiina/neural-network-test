8d8a8abcce464c34f6a8b473a895ca6b53ecad29
Lassi Rintala <lassi.rintala@gmail.com>
2021-10-02 17:26:47 +0300

    Improved MNIST PNG extraction script

--- extract_mnist_png.ps1
+++ extract_mnist_png.ps1
@@ -1,4 +1,9 @@
-$pathTo7zip = "D:\Program Files\7-Zip\7z.exe"
+$pathTo7zip = "C:\Program Files\7-Zip\7z.exe"
+
+if (!(Test-Path -Path $pathTo7zip))
+{
+    $pathTo7zip = "D:\Program Files\7-Zip\7z.exe"
+} 
 
 # Extract the package
 & $pathTo7zip x "external/mnist_png/mnist_png.tar.gz"
